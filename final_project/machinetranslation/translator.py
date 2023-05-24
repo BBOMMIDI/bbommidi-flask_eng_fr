67223d8aa9094c6f4713887fa2ed53d296b61e14
BBOMMIDI <bhanu.bommidi@gmail.com>
2023-05-24 11:41:51 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -15,12 +15,14 @@ language_translator.set_service_url(url)
 
 def english_to_french(english_text):
     """ English to French Translation """
+    if english_text is None: return None
     translation = language_translator.translate(text=english_text,model_id='en-fr').get_result()
     french_text = translation["translations"][0].get("translation")
     return french_text
 
 def french_to_english(french_text):
     """ French to English Translation """
+    if french_text is None: return None
     translation = language_translator.translate(text=french_text,model_id='fr-en').get_result()
     english_text = translation["translations"][0].get("translation")
     return english_text
